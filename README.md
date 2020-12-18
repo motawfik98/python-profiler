441dce5ae0021742ed30460429b4335c916c2b8e
Mohamed Tawfik <motawfik98@outlook.com>
2020-12-18 16:29:07 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+#**_Requirements_**
+This script reads the C++ code given in the code.txt file and generates an output.cpp file which is then compiled to produce output.out executable  
+C++ code must be well formatted for the python script to be able to parse it correctly  
+##**_~~NOT Valid~~_**  
+Function return another function, example:
+
+    int f0() {
+        return f1();
+    }
+Output example:  
+    
+    int f0() {
+    cout << "f1 called" << endl;
+        return f1();
+    cout << "f1 returned" << endl;
+    }
+As this will generate the instrumentation code AFTER the return line which won't get printed  
+Function's curley brackets are at the same line, example:  
+`int f0() { return 5; }`  
+Output example:  
+
+    cout << "f0 called" << endl;
+    int f0() { return 5; }
+    cout << "f0 returned" << endl;
+    
+As this will generate instrumentation code outside the function scope (brackets)  
+Function is called without spaces before or after any special characters, example:  
+`int x=f0();`  
+Output example:
+
+    cout << "x=f0 called" << endl;
+    int x=f0();
+    cout << "x=f0 returned" << endl;
+As `x=f0` will be considered a new function instead of `f0` only  
+##**_Corrections_**
+First case:
+
+    int f0() {
+        int x = f1();
+        return x;
+    }
+Second Case:  
+
+    int f0() {
+        return 5;
+    }
+
+Last Case:
+
+    int x = f0();
---
+++ main.py
@@ -0,0 +1,38 @@
+import os
+
+file = open('code.txt', 'r')
+outputFile = open('output.cpp', 'w')
+lines = file.readlines()
+
+returnedTypes = ['int', 'float', 'double', 'string', 'char', 'bool', 'void']
+
+newFunctionName = False
+writeReturnLine = False
+functionsNames = []
+returnedFunctions = []
+possibleFunctionName = ""
+for line in lines:  # read file line by line
+    for word in line.split(" "):  # read line word by word
+        if "(" in word:  # if a '(' is found in the word
+            possibleFunctionName = word.split("(")[0] + "("
+            if [funcName for funcName in functionsNames if(funcName in possibleFunctionName)]:
+                # check if the current word is present in the saved functions names
+                outputFile.write("""cout << \"""" + possibleFunctionName[:-1] + """ called\" << endl;\n""")
+                returnedFunctions.append("cout << \"" + possibleFunctionName[:-1] + " returned\" << endl;\n")
+                writeReturnLine = True
+        if newFunctionName:  # if it's an indicator that a new function name could be found
+            if "(" in word:  # if it contains parenthesis, then it's a function
+                functionsNames.append(word.split("(")[0] + "(")   # add function name to the list
+            newFunctionName = False
+        if word in returnedTypes:  # if a return type was found, it could be a function
+            newFunctionName = True
+    outputFile.write(line)  # write the original line
+    outputFile.writelines(returnedFunctions)  # write the returned functions logs
+    returnedFunctions = []
+
+file.close()
+outputFile.close()
+if os.system('g++ output.cpp -o output.out') == 0:
+    os.system("./output.out")
+else:
+    print("Unable to compile and run output file")
